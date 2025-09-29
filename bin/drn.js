c4fc31fd4398ca8d7fc54ed8e39f49a3fd8cd4f6
Mauricio-100 <ceoseshell@gmail.com>
2025-09-29 15:18:42 +0100

    Create drn.js

---
+++ bin/drn.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/**
+ * Point d'entrée pour la commande 'drn' en ligne de commande.
+ * Ce script est exécuté chaque fois qu'un utilisateur tape 'drn' dans son terminal.
+ * Son seul rôle est d'importer et d'exécuter le fichier principal du client CLI.
+ */
+
+import '../cli.js';
